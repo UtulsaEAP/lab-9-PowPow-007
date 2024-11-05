e0737ad6a69219d42536e1f20c0dd0479ee1de8a
PowPow-007 <hp@sextonsanders.com>
2024-11-05 14:31:08 -0600

    middle of prob 6

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,8 +1,19 @@
 def fibonacci(n):
     
     #write your code here
-    return
+    a,b = 0,1
+    if n < 0:
+        return -1
+    elif n == 0:
+        return 0
+    else:
+        for _ in range(1, n+1):
+            a,b = b,a+b
+            return b
+
 
 if __name__ == '__main__':
     start_num = int(input())
+    if start_num < 0:
+        print('-1')
     print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
